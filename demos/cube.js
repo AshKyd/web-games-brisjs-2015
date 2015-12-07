71b537d3d85a8b2f926b897defd34b96c1931205
Ash Kyd <ash@kyd.com.au>
2015-12-07 17:34:44 +1000

    Tweaks for brisjs

--- demos/cube.js
+++ demos/cube.js
@@ -1,13 +1,13 @@
 // Colour adjustment function
 // Nicked from http://stackoverflow.com/questions/5560248
 function shadeColor(color, percent) {
-  color = color.substr(1);
-  var num = parseInt(color, 16),
-    amt = Math.round(2.55 * percent),
-    R = (num >> 16) + amt,
-    G = (num >> 8 & 0x00FF) + amt,
-    B = (num & 0x0000FF) + amt;
-  return '#' + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 + (B < 255 ? B < 1 ? 0 : B : 255)).toString(16).slice(1);
+    color = color.substr(1);
+    var num = parseInt(color, 16),
+        amt = Math.round(2.55 * percent),
+        R = (num >> 16) + amt,
+        G = (num >> 8 & 0x00FF) + amt,
+        B = (num & 0x0000FF) + amt;
+    return '#' + (0x1000000 + (R < 255 ? R < 1 ? 0 : R : 255) * 0x10000 + (G < 255 ? G < 1 ? 0 : G : 255) * 0x100 + (B < 255 ? B < 1 ? 0 : B : 255)).toString(16).slice(1);
 }
 
 // Draw a cube to the specified specs
@@ -44,44 +44,44 @@ function drawCube(x, y, wx, wy, h, color, ctx) {
     ctx.strokeStyle = shadeColor(color, 60);
     ctx.stroke();
     ctx.fill();
-  }
+}
 
-(function(){
+(function() {
     // Set up our canvas
     var canvas = document.querySelector('.cube canvas');
-    canvas.width = window.innerWidth*.5;
-    canvas.height = canvas.width*.66;
+    canvas.width = window.innerWidth * 0.5;
+    canvas.height = canvas.width * 0.66;
     var ctx = canvas.getContext('2d');
-    // Pick out the form elements for easy access later
 
+    // Pick out the form elements for easy access later
     var x1 = document.querySelector('#x1');
     var x2 = document.querySelector('#x2');
     var y = document.querySelector('#y');
     var color = document.querySelector('#color');
 
     // Animation function
-    function draw(){
-        if(canvas.class !== 'active'){
+    function draw() {
+        if (canvas.class !== 'active') {
             return requestAnimationFrame(draw);
         }
-      // clear the canvas
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-      // Wobble the cube using a sine wave
-      var wobble = Math.sin(Date.now()/250)*canvas.width/50;
+        // Wobble the cube using a sine wave
+        var wobble = Math.sin(Date.now() / 250) * canvas.width / 50;
 
-      // draw the cube
-      drawCube(
-        canvas.width/2,
-        canvas.height/2 + wobble + y.value/2,
-        Number(x1.value),
-        Number(x2.value),
-        Number(y.value),
-        color.value,
-        ctx
-      );
+        // draw the cube
+        drawCube(
+            canvas.width / 2,
+            canvas.height / 2 + wobble + y.value / 2,
+            Number(x1.value),
+            Number(x2.value),
+            Number(y.value),
+            color.value,
+            ctx
+        );
 
-      requestAnimationFrame(draw);
+        requestAnimationFrame(draw);
     }
     draw();
 })();
